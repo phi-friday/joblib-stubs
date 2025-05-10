af4cd1fa3bff39068a8f9ea67ac3e95691a7b922
phi <phi.friday@gmail.com>
2025-05-10 21:25:43 +0900

    fix: init

--- src/joblib-stubs/__init__.pyi
+++ src/joblib-stubs/__init__.pyi
@@ -1,30 +1,31 @@
 from joblib._cloudpickle_wrapper import (
     wrap_non_picklable_objects as wrap_non_picklable_objects,
 )
-from joblib.compressor import register_compressor as register_compressor
-from joblib.hashing import hash as hash  # noqa: A004
-from joblib.logger import Logger as Logger
-from joblib.logger import PrintTime as PrintTime
-from joblib.memory import MemorizedResult as MemorizedResult
-from joblib.memory import Memory as Memory
-from joblib.memory import expires_after as expires_after
-from joblib.memory import register_store_backend as register_store_backend
-from joblib.numpy_pickle import dump as dump
-from joblib.numpy_pickle import load as load
-from joblib.parallel import Parallel as Parallel
-from joblib.parallel import cpu_count as cpu_count
-from joblib.parallel import delayed as delayed
-from joblib.parallel import effective_n_jobs as effective_n_jobs
-from joblib.parallel import parallel_backend as parallel_backend
-from joblib.parallel import parallel_config as parallel_config
-from joblib.parallel import register_parallel_backend as register_parallel_backend
+from joblib._parallel_backends import ParallelBackendBase
+from joblib._store_backends import StoreBackendBase
+from joblib.compressor import register_compressor
+from joblib.hashing import hash  # noqa: A004
+from joblib.logger import Logger, PrintTime
+from joblib.memory import MemorizedResult, Memory, expires_after, register_store_backend
+from joblib.numpy_pickle import dump, load
+from joblib.parallel import (
+    Parallel,
+    cpu_count,
+    delayed,
+    effective_n_jobs,
+    parallel_backend,
+    parallel_config,
+    register_parallel_backend,
+)
 
 __all__ = [
     "Logger",
     "MemorizedResult",
     "Memory",
     "Parallel",
+    "ParallelBackendBase",
     "PrintTime",
+    "StoreBackendBase",
     "cpu_count",
     "delayed",
     "dump",
